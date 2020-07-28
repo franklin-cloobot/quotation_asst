90339462ca65e2df136f961fe4c8df5995ad0fd5
franklin-cloobot <franklin@cloobot.com>
2020-07-28 19:25:41 +0530

    Update server.py

--- Monolithic/server.py
+++ Monolithic/server.py
@@ -85,6 +85,6 @@ def getwhatsappmessage():
 def start_whatsapp_conversation_server():
     # app.run(port=WHATSAPP_SERVER_PORT,host="0.0.0.0",use_reloader=False)
     application = app
-
+    print("\n\n",__name__,"\n\n")
     if __name__ == '__main__':
         app.run()
