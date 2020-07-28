9acdd4b5801c1b4fb0a4be2226c5a7aa4e1bde8d
franklin-cloobot <franklin@cloobot.com>
2020-07-28 13:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@
 #     # start_conversation()
 # except Exception as e:
 #     print(e)
-
+import sys
+# print(sys.version)
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!"
+    return "Hello, I love Digital Ocean!     "+str(sys.version)
 if __name__ == "__main__":
     app.run()
