727dcc11bec839f79c7f7e5a95e489b014f27dd5
franklin-cloobot <franklin@cloobot.com>
2020-07-28 13:06:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@
 import sys
 # print(sys.version)
 from flask import Flask
-from Monolithic.main import start_conversation
+import * from Monolithic.main
 app = Flask(__name__)
 @app.route("/")
 def hello():
