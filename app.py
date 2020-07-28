641f2f0f767f779dc14f236f045d9ec4ba483b97
franklin-cloobot <franklin@cloobot.com>
2020-07-28 13:23:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!     "+str(Monolithic.main)
+    return "Hello, I love Digital Ocean! "
 if __name__ == "__main__":
     app.run()
+
+
+
