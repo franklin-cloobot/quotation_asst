6dacf1405abb70e98375511969363806413b7c37
franklin-cloobot <franklin@cloobot.com>
2020-07-28 12:52:20 +0530

    debug 1

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from Monolithic.main import start_conversation
-from Monolithic.server import start_whatsapp_conversation_server
+# from Monolithic.main import start_conversation
+# from Monolithic.server import start_whatsapp_conversation_server
 
-try:
-    start_whatsapp_conversation_server()
-    # start_conversation()
-except Exception as e:
-    print(e)
+# try:
+#     start_whatsapp_conversation_server()
+#     # start_conversation()
+# except Exception as e:
+#     print(e)
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, I love Digital Ocean!"
+if __name__ == "__main__":
+    app.run()
